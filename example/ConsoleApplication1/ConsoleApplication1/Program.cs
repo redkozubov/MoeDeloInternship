a087e52deeb4ff87be5e5079c44a99f2e35998bb
e.redkozubov <eugeneredkozubov@gmail.com>
2016-09-08 19:28:15 +0400

    example commit

---
+++ example/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Delate(1, 0);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static double Delate(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
