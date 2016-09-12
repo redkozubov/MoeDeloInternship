f4b169b691d069dd51a7b06e4276eb6c0164f392
lepkov <lepkow@mail.ru>
2016-09-12 22:28:54 +0300

    Заливаю проект по LINQ

---
+++ LinqExample/LinqExample/GasStation.cs
@@ -0,0 +1,33 @@
+﻿namespace LinqExample
+{
+    /// <summary>
+    /// АЗС
+    /// </summary>
+    class GasStation
+    {
+        /// <summary>
+        /// Компания - владелец АЗС
+        /// </summary>
+        public string Owner { get; set; }
+        /// <summary>
+        /// Улица, на которой расположена АЗС
+        /// </summary>
+        public string  Street { get; set; }
+        /// <summary>
+        /// Цена бензина, коп.
+        /// </summary>
+        public int GasPrice { get; set; }
+        /// <summary>
+        /// Марка бензина
+        /// </summary>
+        public int GasMark { get; set; }
+
+        public GasStation(string own, string str, int prc, int mrk)
+        {
+            Owner = own;
+            Street = str;
+            GasPrice = prc;
+            GasMark = mrk;
+        }
+    }
+}
