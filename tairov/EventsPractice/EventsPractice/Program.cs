1ba4201460acb80f7e96b06ca78be36eeca5c2ed
e.redkozubov <eugeneredkozubov@gmail.com>
2016-09-17 12:18:59 +0400

    замечание Таиров

--- tairov/EventsPractice/EventsPractice/Program.cs
+++ tairov/EventsPractice/EventsPractice/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading;
 
-namespace EventsPractice
+namespace EventsPractice //todo: Добавить пустые строки между методами и классами
 {
     public class Counter : EventArgs
     {
