d704c8db4022b8d1d800267d71c5afd21f9e9519
Anton Tairov <123shuchu@gmail.com>
2016-09-21 16:09:57 +0300

    Практическое задание по обобщениям

---
+++ tairov/GenericsPractice/GenericsPractice/MyList.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+
+namespace GenericsPractice
+{
+    class MyList<T> where T : struct
+    {
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node NextNode { get; set; }
+            public Node PreviousNode { get; set; }
+
+            public Node()
+            {
+                Value = default(T);
+                NextNode = null;
+                PreviousNode = null;
+            }
+
+            public Node(T val)
+            {
+                Value = val;
+                NextNode = null;
+                PreviousNode = null;
+            }
+
+            public Node(T val, Node pNode, Node nNode)
+            {
+                Value = val;
+                NextNode = nNode;
+                PreviousNode = pNode;
+            }
+        }
+
+        private Node firstNode;
+        private Node lastNode;
+        private Node currentNode;
+
+        public T CurrentElement
+        {
+            get
+            {
+                if (currentNode != null)
+                    return currentNode.Value;
+                else
+                    throw new NoMoreElementsException("В спике больше нет элементов");
+            }
+            set
+            {
+                if (currentNode != null)
+                    currentNode.Value = value;
+                else
+                    throw new CurrentElementNonExistent("Попытка присвоить значение несущещствующему элементу");
+            }
+        }
+
+        public void AddToHead(T val)
+        {
+            Node newNode = new Node(val, null, firstNode);
+            firstNode.PreviousNode = newNode;
+            firstNode = newNode;
+        }
+
+        public void Add(T val)
+        {
+            Node newNode = new Node(val, lastNode, null);
+            lastNode.NextNode = newNode;
+            lastNode = newNode;
+        }
+
+        public T GetNext()
+        {
+            currentNode = currentNode.NextNode;
+            return CurrentElement;
+        }
+
+        public void GoToHead()
+        {
+            currentNode = firstNode;
+        }
+
+        public MyList()
+        {
+            firstNode = null;
+            lastNode = null;
+            currentNode = null;
+        }
+
+        public MyList(T val)
+        {
+            firstNode = new Node(val);
+            lastNode = firstNode;
+            currentNode = firstNode;
+        }
+
+        public MyList(IEnumerable<T> values)
+        {
+            foreach (T val in values)
+            {
+                if (firstNode == null)
+                {
+                    firstNode = new Node(val);
+                    lastNode = firstNode;
+                }
+                else
+                {
+                    Node nextNode = new Node(val, lastNode, null);
+                    lastNode.NextNode = nextNode;
+                    lastNode = nextNode;
+                }
+            }
+            GoToHead();
+        }
+    }
+}
---
+++ tairov/GenericsPractice/GenericsPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericsPractice
+{
+    class Program
+    {
+        static int Main()
+        {
+            MyList<int> intList = new MyList<int>(new[] {1, 2, 3, 4, 5});
+            intList.Add(17);
+            intList.AddToHead(19);
+            intList.GoToHead();
+            Console.WriteLine(intList.CurrentElement);
+            try
+            {
+                for (int i = 0; i < 10; ++i)
+                {
+                    Console.WriteLine(intList.GetNext());
+                }
+            }
+            catch (NoMoreElementsException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+        }
+    }
+}
